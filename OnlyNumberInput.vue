763d27f23abdb00af197ac9be771c60ba3a6d2dd
soccc <208098588@qq.com>
2019-03-07 17:36:46 +0800

    Update OnlyNumberInput.vue
    增加 最小值、负数、禁用 参数

--- OnlyNumberInput.vue
+++ OnlyNumberInput.vue
@@ -1,21 +1,29 @@
 <!-- onlynumberinput -->
 <template>
-  <input type="text" v-bind:value="value" v-on:input="cinput($event)" v-on:change="$emit('change')">
+  <input type="text"  v-bind:value="value" v-on:input="cinput($event)" v-on:blur="cblur($event)" v-on:change="$emit('change')" :disabled="disabled">
 </template>
 
 <script>
-
 export default {
 	props:{
 		value:{
-			default:'',
+			
 		},
 		decimal:{
 			default:0
 		},
 		max:{
 			default:0
-		}
+		},
+		min:{
+			default:'*'
+		},
+		isNeg:{
+			default:false
+		},
+		disabled:{
+			default:false
+		},
 	},
   data () {
     return {
@@ -40,15 +48,26 @@ export default {
 			},
       cinput(e){
 					var val = event.target.value;
-					if(this.max && val > this.max){
+					if(this.max && val > Number(this.max)){
 						e.target.value = this.max;
 					}else{
 						e.target.value = this.onlyNum(val,this.decimal);
 					}
           this.$emit('input', e.target.value);
 			},
+			cblur(e){
+				var val = event.target.value;
+				if(this.min !== '*' && val < Number(this.min) && val != ''){
+						e.target.value = this.min;
+				}
+				this.$emit('input', e.target.value);
+			},
 			onlyNum(val,size){
+					var neg = false;
 					val = String(val);
+					if( val.indexOf('-') > -1){
+						neg = true;
+					}
 					val = val.replace(/^0+/, "0");
 					if(size == 0 || !size){
 							val = val.replace(/[^\d]/g, "");
@@ -60,8 +79,13 @@ export default {
 							/^(0+[1-9][0-9.]*)$/.test(val) && (val = val.replace(/0/g, "")),
 							val = val.replace(/[。]+/g, ".").replace(/[^\d.]/g, "").replace(/\.{2,}/g, ".").replace(/^\.\.$/g, "0.").replace(/^\.$/g, "0.").replace(".", "$#$").replace(/\./g, "").replace("$#$", ".").replace(reg, "$1$2.$3")
 					}
-					return val;
+					if(neg && this.isNeg){
+						return '-' + val;
+					}else{
+						return val;
+					}
 			}
   }
 }
+
 </script>
